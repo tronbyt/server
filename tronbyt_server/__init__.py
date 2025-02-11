ac90e6abbfbb44d9478e67deb4e8ad7b66dc377b
Tavis <github@tavis.la>
2025-02-11 09:05:30 -0300

    remove extra users line in __init__

--- tronbyt_server/__init__.py
+++ tronbyt_server/__init__.py
@@ -24,7 +24,6 @@ def create_app(test_config=None):
             MAX_CONTENT_LENGTH=1000 * 1000,  # 1mbyte upload size limit
             SERVER_PROTOCOL=os.getenv("SERVER_PROTOCOL", "http"),
             PIXLET_RENDER_PORT1=os.getenv("PIXLET_RENDER_PORT1", "5100"),
-            USERS_DIR="tests/users",
             DB_FILE="testdb.sqlite",
             SERVER_HOSTNAME="localhost",
             MAIN_PORT=os.environ["SERVER_PORT"] or 8000,
