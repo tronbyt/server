e74438651b740992e30f7622548ffbf65deee2bf
Ingmar Stein <IngmarStein@users.noreply.github.com>
2025-03-01 23:10:20 +0100

    Add a health check

---
+++ healthcheck.py
@@ -0,0 +1,25 @@
+import sys
+import requests
+
+
+def health_check(url: str) -> bool:
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f"Failed: {e}")
+        return False
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python healthcheck.py <URL>")
+        sys.exit(1)
+
+    url: str = sys.argv[1]
+    result: bool = health_check(url)
+
+    sys.exit(0 if result else 1)
