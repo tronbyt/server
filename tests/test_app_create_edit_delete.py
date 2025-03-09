2a76ade576a132233438477c11c14f3829f9bd44
Tavis <github@tavis.la>
2025-03-08 16:38:09 -1000

    configapp test with mocks for pixlet (#130)
    * configapp test with mocks for pixlet
    
    * maybe mocks not needed for gh test ?
    
    * fuck you ruff
    
    * back to pathlib

--- tests/test_app_create_edit_delete.py
+++ tests/test_app_create_edit_delete.py
@@ -1,11 +1,17 @@
 from pathlib import Path
-
 from tronbyt_server import db
-
 from . import utils
+# from unittest.mock import patch
 
 
-def test_app_create_edit_delete(client):
+# @patch("os.system")
+# @patch("subprocess.Popen")
+# def test_app_create_edit_config_delete(mock_os_system, mock_subprocess, client):
+def test_app_create_edit_config_delete(client):
+    # Configure the mock to return a successful result
+    # mock_subprocess.return_value.returncode = 0
+    # mock_os_system.return_value.returncode = 0
+
     client.post("/auth/register", data={"username": "testuser", "password": "password"})
     client.post("/auth/login", data={"username": "testuser", "password": "password"})
     client.post(
@@ -20,26 +26,39 @@ def test_app_create_edit_delete(client):
     )
 
     device_id = utils.get_test_device_id()
+
+    r = client.get(f"/{device_id}/addapp")
+    assert r.status_code == 200
+
     r = client.post(
         f"/{device_id}/addapp",
-        data={"name": "TESTAPP", "uinterval": "69", "display_time": "10", "notes": ""},
+        data={
+            "name": "NOAA Tides",
+            "uinterval": "69",
+            "display_time": "10",
+            "notes": "",
+        },
     )
-    assert "TESTAPP" in utils.get_testuser_config_string()
+    assert "NOAA Tides" in utils.get_testuser_config_string()
     app_id = utils.get_test_app_id()
+
+    r = client.get(f"{device_id}/{app_id}/1/configapp")
+    assert r.status_code == 200
+
     r = client.post(
         f"{device_id}/{app_id}/updateapp",
         data={
             "iname": app_id,
-            "name": "TESTAPPUPDATED",
+            "name": "NOAA Tides",
             "uinterval": "69",
             "display_time": "69",
             "notes": "69",
         },
     )
-    print(r.text)
+    print(r.data.decode())
     test_app_dict = utils.get_test_app_dict()
     print(str(test_app_dict))
-    # assert test_app_dict['name'] == "TESTAPPUPDATED"
+
     assert test_app_dict["uinterval"] == "69"
     assert test_app_dict["display_time"] == 69
     assert test_app_dict["notes"] == "69"
