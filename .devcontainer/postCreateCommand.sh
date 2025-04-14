c5338b6567948e886dd6b3e63ebf704000bd5594
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2025-04-14 18:05:12 +0200

    Use the app output returned by libpixlet
    Requires https://github.com/tronbyt/pixlet/pull/57 to be submitted and
    deployed first.
    
    See https://github.com/tronbyt/server/issues/138

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,7 +3,7 @@
 pip3 install --user -r requirements.txt pytest
 mypy --install-types --non-interactive --ignore-missing-imports --exclude system-apps .
 
-PIXLET_VERSION=v0.41.0
+PIXLET_VERSION=v0.42.0
 curl -LO "https://github.com/tronbyt/pixlet/releases/download/${PIXLET_VERSION}/pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo tar -C /usr/local/bin -xvf "pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo mv /usr/local/bin/libpixlet.so /usr/lib/libpixlet.so
