36b41051bbe8c96ab42df2b8a955aecda390f934
Tavis <github@tavis.la>
2025-08-17 17:09:32 -1000

    use the list of all apps installed by the user for all their devices.

--- tronbyt_server/manager.py
+++ tronbyt_server/manager.py
@@ -466,16 +466,19 @@ def addapp(device_id: str) -> ResponseReturnValue:
         custom_apps_list = db.get_apps_list(g.user["username"])
         apps_list = db.get_apps_list("system")
 
-        # Get the list of apps already installed on this device
-        device = g.user["devices"][device_id]
-        installed_app_names = {app["name"] for app in device.get("apps", {}).values()}
+        # Get the list of apps already installed on all devices for this user
+        installed_app_names = set()
+        for device in g.user.get("devices", {}).values():
+            installed_app_names.update(
+                app["name"] for app in device.get("apps", {}).values()
+            )
 
         # Sort apps_list so that installed apps appear first
         apps_list.sort(key=lambda app: 0 if app["name"] in installed_app_names else 1)
 
         return render_template(
             "manager/addapp.html",
-            device=device,
+            device=g.user["devices"][device_id],
             apps_list=apps_list,
             custom_apps_list=custom_apps_list,
         )
