9fb88a77d7e6d1d6ba1caa7cb6ba80366541b3d9
Colin Weir <colin@radioboy.us>
2025-10-26 18:04:05 -0400

    Fix: Add missing server_version_info to post_edit template context

--- tronbyt_server/routers/auth.py
+++ tronbyt_server/routers/auth.py
@@ -305,10 +305,18 @@ def post_edit(
     firmware_version = None
     if user and user.username == "admin":
         firmware_version = db.get_firmware_version()
+    
+    # Get server version info for all users
+    server_version_info = version.get_version_info()
+    
     return templates.TemplateResponse(
         request,
         "auth/edit.html",
-        {"user": user, "firmware_version": firmware_version},
+        {
+            "user": user,
+            "firmware_version": firmware_version,
+            "server_version_info": server_version_info,
+        },
     )
 
 
