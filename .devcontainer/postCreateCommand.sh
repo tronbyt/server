899c727342f65181e479b657485127fb94732380
Gabe Cook <gabe565@gmail.com>
2025-11-29 19:19:38 -0600

    chore(deps): update pixlet to v0.49.2 (#529)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,7 +3,7 @@
 pipx install pdm
 pdm sync -d
 
-PIXLET_VERSION=v0.49.1
+PIXLET_VERSION=v0.49.2
 curl -LO "https://github.com/tronbyt/pixlet/releases/download/${PIXLET_VERSION}/pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo tar -C /usr/local/bin -xvf "pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo mv /usr/local/bin/libpixlet.so /usr/lib/libpixlet.so
