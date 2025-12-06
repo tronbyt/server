d80446c5e04fa9e8fc6905fdcb9a78fe38590272
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2025-12-06 11:39:11 +0100

    deps(pixlet): bump pixlet to 0.49.4

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,7 +3,7 @@
 pipx install pdm
 pdm sync -d
 
-PIXLET_VERSION=v0.49.3
+PIXLET_VERSION=v0.49.4
 curl -LO "https://github.com/tronbyt/pixlet/releases/download/${PIXLET_VERSION}/pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo tar -C /usr/local/bin -xvf "pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo mv /usr/local/bin/libpixlet.so /usr/lib/libpixlet.so
