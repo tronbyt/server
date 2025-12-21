5018b9135bbf9bdd6ad0926757aa3c8d2c78e9f3
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2025-12-22 00:19:48 +0100

    chore: remove unused script

--- scripts/i18n/extract.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Extracts translation strings from source code.
-# Requires goi18n: go install github.com/nicksnyder/go-i18n/v2/goi18n@latest
-
-# Note: goi18n extract primarily works on Go source files.
-# For templates, you might need to manually ensure keys match or use a custom extractor.
-
-goi18n extract -sourceLanguage en -outdir web/i18n/ .
