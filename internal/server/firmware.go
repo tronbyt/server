dee4096600873c8d20c72fa8fda08ea816657287
Tavis <github@tavis.la>
2026-01-26 23:05:14 -1000

    patch: firmware filenaming for S3
    tronbyt-s3 firmware was getting overwritten by matrixportal files

--- internal/server/firmware.go
+++ internal/server/firmware.go
@@ -149,8 +149,8 @@ func (s *Server) UpdateFirmwareBinaries() error {
 		"pixoticker_firmware.bin":                "pixoticker.bin",
 		"tronbyt-s3_firmware.bin":                "tronbyt-S3.bin",
 		"tronbyt-s3-wide_firmware.bin":           "tronbyt-s3-wide.bin",
-		"matrixportal-s3_firmware.bin":           "tronbyt-S3.bin",
-		"matrixportal-s3-waveshare_firmware.bin": "tronbyt-S3.bin",
+		"matrixportal-s3_firmware.bin":           "matrixportal-s3.bin",
+		"matrixportal-s3-waveshare_firmware.bin": "matrixportal-s3-waveshare.bin",
 		// Merged binaries (bootloader + partition + app, flashable at 0x0)
 		"tidbyt-gen1_merged.bin": "tidbyt-gen1_merged.bin",
 		"tronbyt-s3_merged.bin":  "tronbyt-S3_merged.bin",
