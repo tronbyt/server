06ee223f75e0b95031ea228ae3402c6fcf8066e2
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-03-29 08:50:53 +0200

    chore: update devcontainer to Go 1.26 and pixlet v0.52.0

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,7 @@
 
 go mod download
 
-PIXLET_VERSION=v0.49.7
+PIXLET_VERSION=v0.52.0
 curl -LO "https://github.com/tronbyt/pixlet/releases/download/${PIXLET_VERSION}/pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 sudo tar -C /usr/local/bin -xvf "pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
 rm "pixlet_${PIXLET_VERSION}_linux_amd64.tar.gz"
